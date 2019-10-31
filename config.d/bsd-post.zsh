3e5c7bce36ff08438f9350f9a4ea5237135ce174
Thiago Alves <thiagoa@amazon.com>
2019-10-31 11:59:58 -0500

    📄 Add MIT license to file headers

--- config.d/bsd-post.zsh
+++ config.d/bsd-post.zsh
@@ -1,9 +1,32 @@
+# -*- mode: sh; indent-tabs-mode: nil; tab-width: 4; tab-always-indent: t -*-
+
+# MIT License
+#
+# Copyright (c) 2016 Thiago Alves
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 # # --------------------------------------------------------------------
 # # Configuration file for Z Shell Specific for Linux system
 # # By: Thiago Alves
 # # Last Update: August, 16 2019
 # # --------------------------------------------------------------------
-
 
 # # --------------------------------------------------------------------
 # # Contents:
@@ -24,28 +47,20 @@
 # # --------------------------------------------------------------------
 # Shell functions specific for FreeBSD
 
-
-
 # # --------------------------------------------------------------------
 # # 4. Applications
 # # --------------------------------------------------------------------
 # Configuration for applications specific for FreeBSD
-
-
 
 # # --------------------------------------------------------------------
 # # 7. Command Completion
 # # --------------------------------------------------------------------
 # Command completion options specific for FreeBSD
 
-
-
 # # --------------------------------------------------------------------
 # # 8. Aliases
 # # --------------------------------------------------------------------
 # Aliases specific for FreeBSD
-
-
 
 # # --------------------------------------------------------------------
 # # 9. Key Bindings (must be one of the last things in the config)
@@ -53,4 +68,3 @@
 # Bindings specific for FreeBSD. This is also the place to fix bindings
 # that were misconfigured due to any option/configuration in the system
 # specific section.
-
--- config.d/macos-post.zsh
+++ config.d/macos-post.zsh
@@ -1,9 +1,32 @@
+# -*- mode: sh; indent-tabs-mode: nil; tab-width: 4; tab-always-indent: t -*-
+
+# MIT License
+#
+# Copyright (c) 2016 Thiago Alves
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 # # --------------------------------------------------------------------
 # # Configuration file for Z Shell Specific for macOS system
 # # By: Thiago Alves
 # # Last Update: August, 16 2019
 # # --------------------------------------------------------------------
-
 
 # # --------------------------------------------------------------------
 # # Contents:
@@ -24,12 +47,10 @@
 # # --------------------------------------------------------------------
 # Shell functions specific for macOS
 
-function fcd {
-    pFinder=`osascript -e 'tell application "Finder" to if (count of Finder windows) > 0 then get POSIX path of (target of front Finder window as text)'`
+function fcd() {
+    pFinder=$(osascript -e 'tell application "Finder" to if (count of Finder windows) > 0 then get POSIX path of (target of front Finder window as text)')
     [ -n "${pFinder}" ] && cd "${pFinder}"
 }
-
-
 
 # # --------------------------------------------------------------------
 # # 4. Applications
@@ -37,8 +58,8 @@ function fcd {
 # Configuration for applications specific for macOS
 
 # Homebrew
-if [[ -a /usr/local/bin/brew ]]; then
-    BREW_PATH="`/usr/local/bin/brew --prefix`"
+if [[ -e /usr/local/bin/brew ]]; then
+    BREW_PATH="$(/usr/local/bin/brew --prefix)"
     ## add Homebrew to path
     PATH=${PATH}:${BREW_PATH}/share/npm/bin:${BREW_PATH}/opt/sqlite/bin:${BREW_PATH}/sbin
     ## fix man paths
@@ -58,14 +79,14 @@ if [[ -a /usr/local/bin/brew ]]; then
     export HOMEBREW_GITHUB_API_TOKEN=$(gpg_decrypt ${ZDOTDIR:-$HOME}/.secrets/github.api.homebrew)
 fi
 
-
+# NVM
+[ -s "${HOMEBREW_PREFIX}/opt/nvm/nvm.sh" ] && . "${HOMEBREW_PREFIX}/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "${HOMEBREW_PREFIX}/opt/nvm/etc/bash_completion" ] && . "${HOMEBREW_PREFIX}/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion
 
 # # --------------------------------------------------------------------
 # # 7. Command Completion
 # # --------------------------------------------------------------------
 # Command completion options specific for macOS
-
-
 
 # # --------------------------------------------------------------------
 # # 8. Aliases
@@ -81,12 +102,9 @@ alias notify='/Applications/Terminal\ Notifier.app/Contents/MacOS/terminal-notif
 # Quick View
 alias qlf='qlmanage -p "$@" > /dev/null 2>&1'
 
-
-
 # # --------------------------------------------------------------------
 # # 9. Key Bindings (must be one of the last things in the config)
 # # --------------------------------------------------------------------
 # Bindings specific for macOS. This is also the place to fix bindings
 # that were misconfigured due to any option/configuration in the system
 # specific section.
-
